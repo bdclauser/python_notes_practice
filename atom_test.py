4c481481ff0596470d446abf7be8fb7d5946f6de
bdclauser <pzljug@hotmail.com>
2019-08-01 21:59:44 -0400

    work with variables and print outs

--- atom_test.py
+++ atom_test.py
@@ -15,12 +15,34 @@ print 'Line 12', 5.0/2
 # centimeters = 100            one meter is 100 centimeters
 # nanosecond = 1.0/1000000000  one billionth of a second
 
-speed_of_light = 299792458
-centimeters = 100
-nanosecond = 1.0/1000000000
+# Variables
+speed_of_light = 299792458.0        # meters per second
+centimeters = 100                   # one meter is 100 centimeters
+nanosecond = 1.0/1000000000         # one billionth of a second
+cycles_per_second = 2700000000.0    # 2.7 GHz
 
 print 'Line 22', speed_of_light * centimeters * nanosecond
 
 print 'Line 24', 299792458 * 100 * 1.0/1000000000 * 1/2.7
 
 # control + r
+
+
+# print out distance in meters, that light travels in a processor cycle.
+
+cycles_distance = speed_of_light / cycles_per_second
+
+cycles_per_second = 2800000000. # 2.8 GHz
+print cycles_distance
+
+print 'Line 35', 'Cycle Distance = ', speed_of_light / cycles_per_second
+
+cycles_distance = speed_of_light / cycles_per_second
+print cycles_distance
+
+# What is the value of hours after running this code:
+hours = 9
+hours = hours + 1
+hours = hours * 2
+
+print hours
