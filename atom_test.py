41f4ada1dee70dfd675e10430b72424dc09ac53b
bdclauser <pzljug@hotmail.com>
2019-08-01 21:37:03 -0400

    some basic arithmatic/formula practice

--- atom_test.py
+++ atom_test.py
@@ -3,6 +3,24 @@ import random
 for i in range(5):
     print(random.random())
 
-print 2 + 2
+print 'Line 6', 2 + 2
+print 'Line 7', 2 + 3
+print 'Line 8', 4 - 5
 
-print 2 + 3
+print 'Line 10', 5/2
+
+print 'Line 12', 5.0/2
+
+# speed_of_light = 299792458   meters per second
+# centimeters = 100            one meter is 100 centimeters
+# nanosecond = 1.0/1000000000  one billionth of a second
+
+speed_of_light = 299792458
+centimeters = 100
+nanosecond = 1.0/1000000000
+
+print 'Line 22', speed_of_light * centimeters * nanosecond
+
+print 'Line 24', 299792458 * 100 * 1.0/1000000000 * 1/2.7
+
+# control + r
