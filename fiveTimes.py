43838c0ca87b61eb92869ce7f5477c614f8025c2
Brian Clauser <pzljug@hotmail.com>
2019-09-22 23:50:39 -0400

    updated for loop to include while loop example

--- fiveTimes.py
+++ fiveTimes.py
@@ -1,3 +1,14 @@
+print('For Loop Example:')
+
 print('My name is: ')
 for i in range(5):
-    print('Jimmy Five Times (' +str(i) + ')')
+    print('Jimmy Five Times (' + str(i) + ')')
+
+# while loop example
+print('While Loop Example:')
+
+print('My name is: ')
+j = 0
+while j < 5:
+    print('Jimmy Five Times (' + str(j) + ')')
+    j = j + 1
